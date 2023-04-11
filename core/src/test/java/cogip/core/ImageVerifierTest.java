3d54725c0b14587cac15532cfaa69de107d0a06b
Julien Ponge <jponge@redhat.com>
2023-04-11 22:09:48 +0200

    test: add null checks wrt docker-java API

--- core/src/test/java/cogip/core/ImageVerifierTest.java
+++ core/src/test/java/cogip/core/ImageVerifierTest.java
@@ -12,10 +12,17 @@ class ImageVerifierTest {
     void smoke_test() throws Exception {
         try (ContainerEngineSession session = new ContainerEngineSession()) {
             var ref = new Reference("docker.io", "library/hello-world", "linux");
+
             session.pullImage(ref);
+
             session.verifyImage(ref, img -> {
+
                 ContainerConfig conf = img.getConfig();
+                assertThat(conf).isNotNull();
+
                 RootFS rootFS = img.getRootFS();
+                assertThat(rootFS).isNotNull();
+
                 assertThat(img.getOs()).isEqualTo("linux");
                 assertThat(img.getParent()).isEmpty();
                 assertThat(conf.getCmd()).containsExactly("/hello");
